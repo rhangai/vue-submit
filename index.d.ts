e188516caa8c44464509b1b27da8bdbd50df74c4
rhangai <hangai.renan@gmail.com>
2020-03-07 18:11:02 -0300

    fix: Declaration type

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
 import { VueConstructor } from "vue";
 import { VueSubmitPluginOptions } from "./types/vue-submit";
 
-export type VueSubmit = {
+export type VueSubmitPlugin = {
 	install(vue: VueConstructor, options?: VueSubmitPluginOptions): void;
 };
+
+export const VueSubmit: VueSubmitPlugin;
